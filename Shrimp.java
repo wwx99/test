9d18c50ba6806c1575e6ba802181015628ec8817
wwx99 <1142228709@qq.com>
2019-08-22 00:56:34 +0800

    油焖大虾

---
+++ Shrimp.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Shrimp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("剪虾头");
+	System.out.println("抽虾线");
+	System.out.println("洗虾");
+	System.out.println("准备所以调料");
+	System.out.println("倒油");
+	System.out.println("倒调料");
+	System.out.println("炒虾");
+	for (int i = 1; i <= 3; i++) {
+        System.out.println("第"+i+"次加啤酒");
+        System.out.println("焖");
+    }
+	System.out.println("关火");
+	System.out.println("开吃");
+	}
+}
